22290ec9c173c7bd9c5cb6b5fcaaccd44a55ab74
Pavel Filippi <pavel.filippi@gmail.com>
2022-11-16 22:34:55 +0100

    Refactor basic test to run against "prod" db

--- test/test_query_meteostations.py
+++ test/test_query_meteostations.py
@@ -1,18 +1,56 @@
 import pytest
+from src.main import schema
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from fastapi.testclient import TestClient
+from src.dependencies.context import get_database
+from src.main import app
 
 
-@pytest.mark.asyncio
-async def test_query_zero_stations():
+SQLALCHEMY_DATABASE_URL = "sqlite://"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+Base.metadata.create_all(bind=engine)
+
+
+# app.dependency_overrides[get_database] = override_get_db
+
+
+
+client = TestClient(app)
+
+
+# @pytest.mark.asyncio
+def test_query_zero_stations():
     """ Query returns empty result for empty db """
     query = """
         query TestQuery {
             meteostations {
-                battery_percentage
-                location
+                batteryPercentage
+                location {
+                    lat
+                    long
+                }
             }
         }
     """
 
-    result = await schema.execute(query)
+    response = client.post(
+        "/graphql/",
+        json={"query": query},
+    )
 
-    assert result.data["meteostations"] == []
+    # result = await schema.execute(query)
+    assert response.json() == []
+
+
+
+    # assert result.data["meteostations"] == []
