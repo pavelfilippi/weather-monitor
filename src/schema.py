4aeb3ca2e119e2ed6058a68881a843621cac2881
Pavel Filippi <pavel.filippi@gmail.com>
2022-11-20 21:01:53 +0100

    Add `resource_id` to query/mutations

--- src/schema.py
+++ src/schema.py
@@ -16,13 +16,16 @@ class Location:
 
 @strawberry.type
 class WeatherStation:
+    resource_id: int
     battery_percentage: int
     location: Location
 
     @staticmethod
     def from_model(model: models.WeatherStation) -> "WeatherStation":
         return WeatherStation(
-            battery_percentage=model.battery_percentage, location=Location(lat=model.latitude, long=model.longitude)
+            resource_id=model.id,
+            battery_percentage=model.battery_percentage,
+            location=Location(lat=model.latitude, long=model.longitude),
         )
 
 
@@ -49,6 +52,22 @@ class WeatherStationAlreadyExists:
 
 
 NewWeatherStationOutput = strawberry.union("NewWeatherStationOutput", (WeatherStation, WeatherStationAlreadyExists))
+
+
+@strawberry.type
+class WeatherStationRemoved:
+    removed_resource_id: int
+
+
+@strawberry.type
+class WeatherStationNotFound:
+    resource_id: int
+    message: str = "Weather station not found."
+
+
+RemoveWeatherStationOutput = strawberry.union(
+    "RemoveWeatherStationOutput", (WeatherStationRemoved, WeatherStationNotFound)
+)
 
 
 @strawberry.type
@@ -82,29 +101,23 @@ class Mutation:
         return WeatherStation.from_model(weather_station)
 
     @strawberry.mutation(description="Remove weather station.")
-    async def remove_weather_station(self, info: Info[AppContext, Any], longitude: float, latitude: float) -> None:
+    async def remove_weather_station(self, info: Info[AppContext, Any], resource_id: int) -> RemoveWeatherStationOutput:
         """Delete weather station from DB"""
         async with info.context.db.session() as session:
             query = select(
-                exists(
-                    select(1)
-                    .select_from(models.WeatherStation)
-                    .where(
-                        and_(models.WeatherStation.longitude == longitude, models.WeatherStation.latitude == latitude)
-                    )
-                )
+                exists(select(1).select_from(models.WeatherStation).where(models.WeatherStation.id == resource_id))
             )
             result = await session.execute(query)
             weather_station_exists = result.scalar()
             if weather_station_exists:
-                query = select(models.WeatherStation).where(
-                    and_(models.WeatherStation.longitude == longitude, models.WeatherStation.latitude == latitude)
-                )
+                query = select(models.WeatherStation).where(models.WeatherStation.id == resource_id)
 
                 result = await session.execute(query)
                 weather_station = result.scalar()
 
                 await session.delete(weather_station)
+                return WeatherStationRemoved(removed_resource_id=resource_id)
+            return WeatherStationNotFound(resource_id=resource_id)
 
 
 schema = strawberry.Schema(query=Query, mutation=Mutation)
