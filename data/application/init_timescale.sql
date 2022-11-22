024e883d4c9a53817754d3236ae3674e2ba80fc1
Pavel Filippi <pavel.filippi@gmail.com>
2022-11-22 20:46:46 +0100

    Change init sql file
    - refactor types
    - fix creating index on non-existing field

--- data/application/init_timescale.sql
+++ data/application/init_timescale.sql
@@ -1,19 +1,18 @@
-\
-c weather_monitor;
+\c weather_monitor;
 
 CREATE TABLE IF NOT EXISTS weather_real_time
 (
-    time TIMESTAMPTZ NOT NULL,
-    station_id         INT              NOT NULL,
-    battery_percentage FLOAT            NULL,
-    temperature        DOUBLE PRECISION NULL,
-    humidity           DOUBLE PRECISION NULL,
-    pressure           DOUBLE PRECISION NULL
+    time               TIMESTAMP NOT NULL,
+    station_id         INT       NOT NULL,
+    battery_percentage FLOAT     NULL,
+    temperature        FLOAT     NULL,
+    humidity           FLOAT     NULL,
+    pressure           FLOAT     NULL
 );
 
 SELECT create_hypertable('weather_real_time', 'time');
 
-CREATE INDEX ix_station_id_time ON weather_real_time (id, time DESC);
+CREATE INDEX ix_station_id_time ON weather_real_time (station_id, time DESC);
 
 CREATE TABLE IF NOT EXISTS weather_stations
 (
