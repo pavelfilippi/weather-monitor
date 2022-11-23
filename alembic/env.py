0713c223378777be01e0d5f26462a93ee38dae78
Pavel Filippi <pavel.filippi@gmail.com>
2022-11-23 22:33:31 +0100

    Configure alembic for autogenerate migrations

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,7 @@ from alembic import context
 from dotenv import load_dotenv
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from src.models import Base
 
 load_dotenv()
 
@@ -32,7 +33,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
