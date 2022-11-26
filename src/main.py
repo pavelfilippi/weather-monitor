73bd13f36b6f4b215de7697ac3968de7a21bdcde
Pavel Filippi <pavel.filippi@gmail.com>
2022-11-26 21:11:43 +0100

    Copy & simplify https://fastapi.tiangolo.com/tutorial/security/simple-oauth2/

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from strawberry.fastapi import GraphQLRouter
 from src.dependencies.context import AppContext
 from src.schema import schema
 from src import weather_conditions
+from src import user_management
 
 app = FastAPI()
 
@@ -11,3 +12,4 @@ app = FastAPI()
 graphql_app = GraphQLRouter(schema, context_getter=AppContext)
 app.include_router(graphql_app, prefix="/graphql")
 app.include_router(weather_conditions.router, prefix="")
+app.include_router(user_management.router, prefix="")
---
+++ src/user_management.py
@@ -0,0 +1,68 @@
+from fastapi import Depends, HTTPException, status, APIRouter
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "password": "secret",
+    },
+    "alice": {
+        "username": "alice",
+        "password": "secret2",
+    },
+}
+
+router = APIRouter()
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class User(BaseModel):
+    username: str
+
+
+class UserInDB(User):
+    password: str
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def fake_decode_token(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(fake_users_db, token)
+    return user
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+@router.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    if not form_data.password == user.password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@router.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
